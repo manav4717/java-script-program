7f8f59a38eed1ced922209a0659a40520fc4415e
Manav Goswami <manavbharthigoswami29@gmail.com>
2026-01-28 13:13:20 +0530

    [MG][file added]

---
+++ variable.js
@@ -0,0 +1,72 @@
+/*function adds(a,b)
+{
+    console.log(a+b);
+}
+adds(5,9)
+function sub(a,b)
+{
+    console.log(a-b);
+}sub(5,9)
+function mult(a,b)
+{
+    console.log(a*b);
+}
+mult(5,9)
+function dis(a,b)
+{
+    console.log(a/b);
+}
+dis(5,9)
+
+const adds=(a=10,b=50)=>{
+    return a+b
+}
+console.log(adds())
+const sub=(a=10,b=50)=>{
+    return a-b
+}
+console.log(sub())
+const mult=(a=10,b=50)=>{
+    return a*b
+}
+console.log(mult())
+const dis=(a=10,b=50)=>{
+    return a/b
+}
+console.log(dis())
+let value = 12;
+console.log('default value',value);
+
+value +=5
+console.log(value)
+value -=5
+console.log(value)
+value *=5
+console.log(value)
+value /=5
+console.log(value)
+let age=20;
+let hasid=true
+let name = "student"
+let obj={
+    name:'example',
+    rollno:23
+}
+
+console.log("logical and (&&):",age>18 && hasid);
+console.log("logical or (||):",age<18 || hasid)
+console.log("logical not (!):",! hasid)
+  
+
+console.log(typeof age)
+console.log(typeof hasid)
+console.log(typeof name)
+console.log(typeof obj)*/
+
+let value=10;
+let num="10"
+
+console.log("value == num :",value==num);
+console.log("value === num :",value===num);
+console.log("value !num :",value !=num)
+
